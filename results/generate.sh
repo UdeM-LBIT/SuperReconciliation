a13d29929fe237dd7e28319b3558388138c44fd8
Mattéo Delabre <contact@matteodelabre.me>
2018-06-15 18:54:46 -0400

    Update simulation generator script

--- results/generate.sh
+++ results/generate.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-build="$1"
-src="$2"
-dest_in="$src.in.pdf"
-dest_out="$src.out.pdf"
+build="../build"
+dir="simulation/$RANDOM"
+mkdir -p "$dir"
 
-"$build/viz" < "$src" | dot -Tpdf > "$dest_in"
-"$build/main" < "$src" | "$build/viz" | dot -Tpdf > "$dest_out"
+echo "--- Generating a simulated tree ---"
+echo "Directory: $dir"
+simulation_results="$("$build/simulate")"
+echo ""
 
-xdg-open "$dest_in"
-xdg-open "$dest_out"
+original="$(echo "$simulation_results" | head -1)"
+echo "$original" > "$dir/original"
+"$build/viz" < "$dir/original" | dot -Tpdf > "$dir/original.pdf"
+
+erased="$(echo "$simulation_results" | tail -1)"
+echo "$erased" > "$dir/erased"
+"$build/viz" < "$dir/erased" | dot -Tpdf > "$dir/erased.pdf"
+
+"$build/super_reconciliation" < "$dir/erased" > "$dir/reconciled"
+"$build/viz" < "$dir/reconciled" | dot -Tpdf > "$dir/reconciled.pdf"
