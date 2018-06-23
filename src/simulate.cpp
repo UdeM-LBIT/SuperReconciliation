2ee6935b6af1b1ffca13db0d3587e654e071041c
Mattéo Delabre <contact@matteodelabre.me>
2018-06-23 13:45:22 -0400

    Simulate: report progress & improve work distrib
    Do not dedicate each thread to a set of params because some settings
    take much longer than others. Instead, enable any thread to take on any
    sample task.

--- src/simulate.cpp
+++ src/simulate.cpp
@@ -5,10 +5,12 @@
 #include "util/MultivaluedNumber.hpp"
 #include <boost/program_options.hpp>
 #include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <nlohmann/json.hpp>
 #include <omp.h>
-#include <ostream>
-#include <fstream>
+#include <unordered_map>
 #include <vector>
 
 namespace po = boost::program_options;
@@ -28,10 +30,11 @@ struct EvaluationResults
 
 template<typename PRNG>
 void evaluate(
+    PRNG& prng,
     EvaluationResults& results,
-    EvolutionParams<PRNG>& params)
+    EvolutionParams& params)
 {
-    auto reference_tree = simulate_evolution(params);
+    auto reference_tree = simulate_evolution(prng, params);
     auto reconciled_tree = reference_tree;
     erase_tree(reconciled_tree, std::begin(reconciled_tree));
 
@@ -65,55 +68,6 @@ void evaluate(
 
         results.scoredif = ref_score - rec_score;
     }
-}
-
-template<typename PRNG>
-json sample(
-    EvolutionParams<PRNG>& params,
-    unsigned sample_size,
-    bool needs_scoredif,
-    bool needs_duration)
-{
-    json results = {
-        {"params", {
-            {"synteny_size", params.base_synteny.size()},
-            {"event_depth", params.event_depth},
-            {"duplication_probability", params.duplication_probability},
-            {"loss_probability", params.loss_probability},
-            {"loss_length_rate", params.loss_length_rate}
-        }}
-    };
-
-    if (needs_scoredif)
-    {
-        results["scoredif"] = json::array();
-    }
-
-    if (needs_duration)
-    {
-        results["duration"] = json::array();
-    }
-
-    EvaluationResults sample_info;
-    sample_info.needs_scoredif = needs_scoredif;
-    sample_info.needs_duration = needs_duration;
-
-    for (unsigned i = 0; i < sample_size; ++i)
-    {
-        evaluate(sample_info, params);
-
-        if (needs_scoredif)
-        {
-            results["scoredif"].push_back(sample_info.scoredif);
-        }
-
-        if (needs_duration)
-        {
-            results["duration"].push_back(sample_info.duration);
-        }
-    }
-
-    return results;
 }
 
 struct Arguments
@@ -257,14 +211,19 @@ struct ThreadLifecycle
         std::random_device rd;
         auto seed = rd();
         prng.seed(seed);
-
-        std::cerr << "Thread #" + std::to_string(omp_get_thread_num())
-            + " started with seed: " + std::to_string(seed) + "\n";
     }
 
     // Thread-local pseudo-random number generator
     std::mt19937 prng;
 };
+
+void report_progress(unsigned long performed, unsigned long total)
+{
+    std::cout << std::fixed << std::setprecision(2) << "\r["
+        << std::setw(6) << ((static_cast<double>(performed) / total) * 100)
+        << "%] " << performed << "/" << total << " tasks performed"
+        << "               " << std::flush;
+}
 
 int main(int argc, const char* argv[])
 {
@@ -282,17 +241,31 @@ int main(int argc, const char* argv[])
     }
 
     ThreadLifecycle lifecycle;
-    json result = json::array();
 
-    // Reduction for JSON arrays ([] + [] -> [])
-    #pragma omp declare reduction(jsonarray : json : \
-            omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end())) \
-        initializer(omp_priv = json::array())
+    // Stores all the results for each set of parameters and each sample
+    json results = json::array();
+
+    // For each set of parameters, store the index in the results array
+    // at which measurements are stored
+    std::unordered_map<EvolutionParams, std::size_t> find_params_index;
+
+    // Track task progression
+    unsigned long performed_tasks = 0;
+    unsigned long total_tasks = args.sample_size
+        * args.synteny_size.size()
+        * args.event_depth.size()
+        * args.duplication_probability.size()
+        * args.loss_probability.size()
+        * args.loss_length_rate.size();
+
+    report_progress(performed_tasks, total_tasks);
 
     #pragma omp parallel for \
-        firstprivate(lifecycle, args) default(none) \
-        collapse(5) schedule(dynamic) \
-        reduction(jsonarray : result)
+        firstprivate(lifecycle, args, total_tasks) default(none) \
+        shared(results, find_params_index, performed_tasks) \
+        collapse(6) schedule(dynamic)
+    for (unsigned sample_id = 0; sample_id < args.sample_size; ++sample_id)
+    {
     for (
         auto synteny_size = args.synteny_size.begin();
         synteny_size < args.synteny_size.end();
@@ -321,22 +294,69 @@ int main(int argc, const char* argv[])
         bool needs_scoredif = contains(args.metrics, "scoredif"s);
         bool needs_duration = contains(args.metrics, "duration"s);
 
-        EvolutionParams<std::mt19937> params;
-        params.random_generator = &lifecycle.prng;
-        params.base_synteny = Synteny::generateDummy(*synteny_size);
-        params.event_depth = *event_depth;
-        params.duplication_probability = *duplication_prob;
-        params.loss_probability = *loss_prob;
-        params.loss_length_rate = *loss_length_rate;
+        EvolutionParams sample_params;
+        sample_params.base_synteny = Synteny::generateDummy(*synteny_size);
+        sample_params.event_depth = *event_depth;
+        sample_params.duplication_probability = *duplication_prob;
+        sample_params.loss_probability = *loss_prob;
+        sample_params.loss_length_rate = *args.loss_length_rate;
 
-        json sample_result = sample(
-            params, args.sample_size,
-            needs_scoredif, needs_duration);
+        EvaluationResults sample_info;
+        sample_info.needs_scoredif = needs_scoredif;
+        sample_info.needs_duration = needs_duration;
 
-        result.push_back(sample_result);
-    }}}}}
+        evaluate(lifecycle.prng, sample_info, sample_params);
+
+        #pragma omp critical
+        {
+            // If no entry exist for the current set of params exist, create
+            // one. Otherwise, directly append results to the existing one
+            // by using the stored index
+            if (!find_params_index.count(sample_params))
+            {
+                json sample_result = {
+                    {"params", {
+                        {"synteny_size", *synteny_size},
+                        {"event_depth", *event_depth},
+                        {"duplication_probability", *duplication_prob},
+                        {"loss_probability", *loss_prob},
+                        {"loss_length_rate", *loss_length_rate}
+                    }}
+                };
+
+                if (needs_scoredif)
+                {
+                    sample_result["scoredif"] = json::array();
+                }
+
+                if (needs_duration)
+                {
+                    sample_result["duration"] = json::array();
+                }
+
+                find_params_index.emplace(sample_params, results.size());
+                results.push_back(std::move(sample_result));
+            }
+
+            json& sample_result = results.at(
+                find_params_index.at(sample_params));
+
+            if (needs_scoredif)
+            {
+                sample_result["scoredif"].push_back(sample_info.scoredif);
+            }
+
+            if (needs_duration)
+            {
+                sample_result["duration"].push_back(sample_info.duration);
+            }
+
+            ++performed_tasks;
+            report_progress(performed_tasks, total_tasks);
+        }
+    }}}}}}
 
     std::ofstream output(args.output);
-    output << result;
+    output << results;
     return EXIT_SUCCESS;
 }
