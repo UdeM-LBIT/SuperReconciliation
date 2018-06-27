74b189f140354851193c945c0522f632b3e7a50b
Mattéo Delabre <contact@matteodelabre.me>
2018-06-27 17:24:27 -0400

    Add results files

--- tools/plot.sh
+++ tools/plot.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-./plot.py output/by-depth.json \
+./plot.py results/by-depth.json \
     event_depth duration \
     output/simulation-time-by-depth.pdf \
     --fit-poly=4
-./plot.py output/by-depth.json \
+./plot.py results/by-depth.json \
     event_depth duration \
     output/simulation-time-by-depth.pgf \
     --fit-poly=4
 
-./plot.py output/by-length.json \
+./plot.py results/by-length.json \
     synteny_size duration \
     output/simulation-time-by-length.pdf \
     --fit-exp --semilog-y
-./plot.py output/by-length.json \
+./plot.py results/by-length.json \
     synteny_size duration \
     output/simulation-time-by-length.pgf \
     --fit-exp --semilog-y
