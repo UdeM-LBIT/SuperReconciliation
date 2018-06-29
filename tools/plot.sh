0907644ccee2cdce675104046c588378356a00d3
Mattéo Delabre <contact@matteodelabre.me>
2018-06-29 11:27:26 -0400

    Update results as generated

--- tools/plot.sh
+++ tools/plot.sh
@@ -2,12 +2,17 @@
 
 ./plot.py results/by-depth.json \
     event_depth duration \
-    output/simulation-time-by-depth.pdf \
-    --fit-poly=4
+    output/simulation-time-by-depth.pdf
 ./plot.py results/by-depth.json \
     event_depth duration \
-    output/simulation-time-by-depth.pgf \
-    --fit-poly=4
+    output/simulation-time-by-depth.pgf
+
+./plot.py results/by-depth.json \
+    event_depth dlscore \
+    output/simulation-dlscore-by-depth.pdf
+./plot.py results/by-depth.json \
+    event_depth dlscore \
+    output/simulation-dlscore-by-depth.pgf
 
 ./plot.py results/by-length.json \
     synteny_size duration \
@@ -17,3 +22,10 @@
     synteny_size duration \
     output/simulation-time-by-length.pgf \
     --fit-exp --semilog-y
+
+./plot.py results/by-length.json \
+    synteny_size dlscore \
+    output/simulation-dlscore-by-length.pdf
+./plot.py results/by-length.json \
+    synteny_size dlscore \
+    output/simulation-dlscore-by-length.pgf
