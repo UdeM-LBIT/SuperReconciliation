91168e5b9a99d1d2ae4e5a412dfa48a4f936151a
Mattéo Delabre <contact@matteodelabre.me>
2018-08-28 00:40:44 -0400

    Add measurements by synteny length for USR

--- tools/plot.sh
+++ tools/plot.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-./plot.py results/by-depth.json \
-    event_depth duration \
-    output/simulation-time-by-depth.pdf
-./plot.py results/by-depth.json \
-    event_depth duration \
-    output/simulation-time-by-depth.pgf
+# Generate the plots used in the paper
 
-./plot.py results/by-depth.json \
-    event_depth dlscore \
-    output/simulation-dlscore-by-depth.pdf
-./plot.py results/by-depth.json \
-    event_depth dlscore \
-    output/simulation-dlscore-by-depth.pgf
+./plot.py results/ordered-by-depth.json \
+    depth duration \
+    output/ordered-time-by-depth.pdf
+./plot.py results/ordered-by-depth.json \
+    depth duration \
+    output/ordered-time-by-depth.pgf
 
-./plot.py results/by-length.json \
-    synteny_size duration \
-    output/simulation-time-by-length.pdf \
+./plot.py results/ordered-by-length.json \
+    base_size duration \
+    output/ordered-time-by-length.pdf \
     --fit-exp --semilog-y
-./plot.py results/by-length.json \
-    synteny_size duration \
-    output/simulation-time-by-length.pgf \
+./plot.py results/ordered-by-length.json \
+    base_size duration \
+    output/ordered-time-by-length.pgf \
     --fit-exp --semilog-y
 
-./plot.py results/by-length.json \
-    synteny_size dlscore \
-    output/simulation-dlscore-by-length.pdf
-./plot.py results/by-length.json \
-    synteny_size dlscore \
-    output/simulation-dlscore-by-length.pgf
+./plot.py results/unordered-by-length.json \
+    base_size duration \
+    output/unordered-time-by-length.pdf \
+    --kind average
+./plot.py results/unordered-by-length.json \
+    base_size duration \
+    output/unordered-time-by-length.pgf \
+    --kind average
